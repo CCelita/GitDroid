f2409ef74fd4e55461ef7418cc9353233a7b07ae
gqq <gqq519@163.com>
2016-09-29 14:57:20 +0800

    项目主页面侧滑实现

--- app/src/main/java/com/feicuiedu/gitdroid/MainActivity.java
+++ app/src/main/java/com/feicuiedu/gitdroid/MainActivity.java
@@ -1,16 +1,84 @@
 package com.feicuiedu.gitdroid;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+import com.feicuiedu.gitdroid.commons.ActivityUtils;
+import com.feicuiedu.gitdroid.login.LoginActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @BindView(R.id.toolbar)
+    Toolbar toolbar;
+    @BindView(R.id.navigationView)
+    NavigationView navigationView;
+    @BindView(R.id.drawerLayout)
+    DrawerLayout drawerLayout;
+    private Button btnLogin;
+    private ImageView ivIcon;
+
+    private ActivityUtils activityUtils;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // 会触发onContentChanged方法
         setContentView(R.layout.activity_main);
+    }
 
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
+        activityUtils = new ActivityUtils(this);
 
+        // 处理toolbar作为ActionBar
+        setSupportActionBar(toolbar);
 
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        toggle.syncState();
+        drawerLayout.addDrawerListener(toggle);
+
+        btnLogin = ButterKnife.findById(navigationView.getHeaderView(0), R.id.btnLogin);
+        ivIcon = ButterKnife.findById(navigationView.getHeaderView(0), R.id.ivIcon);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                activityUtils.startActivity(LoginActivity.class);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        if (item.isChecked()){
+            item.setChecked(false);
+        }
+
+        // 根据不同的item的id来进行切换
+        switch (item.getItemId()){
+
+            // 最热门
+            case R.id.github_hot_repo:
+
+                break;
+        }
+
+        return false;
     }
 }
